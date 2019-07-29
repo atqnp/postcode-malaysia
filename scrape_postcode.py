49c43e1ae1a1c4151dfb6ed81badcbef5ae88f98
atqnp <atiq_np@hotmail.com>
2019-07-29 15:52:45 +0900

    Update scrape_postcode.py

--- scrape_postcode.py
+++ scrape_postcode.py
@@ -2,11 +2,11 @@
 import pandas as pd
 import requests
 
-num = ["%.4d" % i for i in range(0,10000)]
+num = ["%.5d" % i for i in range(0,100000)]
 
 def get_postcode(num):
     headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36'}
-    url = "https://api.pos.com.my/PostcodeWebApi/api/Postcode?Postcode={}0".format(i)
+    url = "https://api.pos.com.my/PostcodeWebApi/api/Postcode?Postcode={}".format(i)
     data = requests.get(url, headers=headers).json()
     return data
 
